2fc7b351ccb3101882ba97b7ffff28b5db0d27e1
saransh-khobragade <saransh98@gmail.com>
2023-01-09 23:55:36 +0530

    fetching env variable from file done

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/api"
+	db "github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/db/sqlc"
+	"github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/db/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	fmt.Print(config)
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+
+}
