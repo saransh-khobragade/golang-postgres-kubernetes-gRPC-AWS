ae8c60eae5522a2944102e85eb82dbcdff31493d
saransh-khobragade <saransh98@gmail.com>
2023-01-12 23:41:07 +0530

    data encryption using bcrypt

--- api/server.go
+++ api/server.go
@@ -23,8 +23,8 @@ func NewServer(store db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
-
 	router.POST("/transfers", server.createTransfer)
+	router.POST("/users", server.createUser)
 
 	server.router = router
 	return server
