d431e36bfc172501c42748fe22e4f867852bc651
saransh-khobragade <saransh98@gmail.com>
2023-02-04 21:08:52 +0530

    Done AWS

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "postgresql://root:YVpMHu4nBJxqkUDMS85a@simple-bank.cev0k397mf6f.ap-south-1.rds.amazonaws.com:5432/simple_bank" -verbose up
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
