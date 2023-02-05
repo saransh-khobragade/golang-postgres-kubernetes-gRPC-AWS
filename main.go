6b0db6d21143017257a3f99787694253d0f773d6
saransh-khobragade <saransh98@gmail.com>
2023-02-05 12:12:28 +0530

    One gateway for http endpoints and grpc endpoint done

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
-	"github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/api"
 	db "github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/db/sqlc"
 	"github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/db/util"
 	"github.com/saransh-khobragade/golang-postgres-kubernetes-gRPC/gapi"
@@ -30,7 +32,9 @@ func main() {
 	}
 	store := db.NewStore(conn)
 	// runGrpcServer(config, store)
-	runGinServer(config, store)
+	// runGinServer(config, store)
+	go runGatewayServer(config, store)
+	runGrpcServer(config, store)
 }
 
 func runGrpcServer(config util.Config, store db.Store) {
@@ -54,15 +58,47 @@ func runGrpcServer(config util.Config, store db.Store) {
 		log.Fatal("cannot start gRPC server", err)
 	}
 }
-
-func runGinServer(config util.Config, store db.Store) {
-	server, err := api.NewServer(config, store)
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
 	if err != nil {
 		log.Fatal("cannot create server", err)
 	}
 
-	err = server.Start(config.HttpServerAddress)
+	grpcMux := runtime.NewServeMux()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
 	if err != nil {
-		log.Fatal("cannot start server", err)
+		log.Fatal("cannot register handler server", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listner, err := net.Listen("tcp", config.HttpServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener", err)
+	}
+
+	log.Printf("start HTTP gateway server started at %s", listner.Addr().String())
+
+	err = http.Serve(listner, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server", err)
 	}
 }
+
+//FOR RUNNING ONLY HTTP ENDPOINTS
+// func runGinServer(config util.Config, store db.Store) {
+// 	server, err := api.NewServer(config, store)
+// 	if err != nil {
+// 		log.Fatal("cannot create server", err)
+// 	}
+
+// 	err = server.Start(config.HttpServerAddress)
+// 	if err != nil {
+// 		log.Fatal("cannot start server", err)
+// 	}
+// }
